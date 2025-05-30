68246a095b13dae20fa4df956f9ae3bd1f06282e
sean-madu <madusean2@gmail.com>
2025-05-30 02:35:16 -0600

    Update types to new schema

--- frontend/hot-takes-hedging/src/types.ts
+++ frontend/hot-takes-hedging/src/types.ts
@@ -1,14 +1,26 @@
 export type InvestmentAdviceItem = {
-    "Ticker Symbol/Asset Name": string;
-    "Class": string;
-    "Strategic Recommendation": string;
-    "Confidence (0 - 10)": number;
-    "Rationale": string;
-    "Future Search Term"?: string[];
-    "Asset explanation"?: string;    
-    "Amount to Invest/Sell"?: string; 
-    "Risk Profile": string;
-  };
-  
-  export type InvestmentAdvice = InvestmentAdviceItem[];
-  
+  "Ticker Symbol/Asset Name": string;
+  "Class": string;
+  "Strategic Recommendation": string;
+  "Confidence (0 - 10)": number;
+  "Rationale": string;
+  "Future Search Term"?: string[];
+  "Asset explanation"?: string;
+  "Change to Portfolio"?: string;
+  "Risk Profile": string;
+};
+
+
+export type Article = { "title": string, "description": string, "url": string }
+
+export type InvestmentAdvice = InvestmentAdviceItem[];
+
+export type InvestmentAdvicePost = {
+  "Investment Advice": InvestmentAdvice,
+  "articles": Article[],
+  "timestamp": string,
+  "hex ID": string,
+}
+
+export type InvestmentAdvicePosts = InvestmentAdvicePost[];
+
