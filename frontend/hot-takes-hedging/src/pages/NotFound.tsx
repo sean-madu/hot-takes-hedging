4fe225b88b0789c34af5b0052cbabd9212ef07bb
sean-madu <madusean2@gmail.com>
2025-05-30 02:33:24 -0600

    Slightly better 404

--- frontend/hot-takes-hedging/src/pages/NotFound.tsx
+++ frontend/hot-takes-hedging/src/pages/NotFound.tsx
@@ -1,6 +1,33 @@
+import { Typography, Stack, Button } from '@mui/material';
 import React from 'react';
+import GlassCard from '../components/GlassCard';
 
-// TODO add stuff for return
-const NotFound: React.FC = () => <h1>404 - Page Not Found</h1>;
+const NotFound: React.FC = () => {
+
+    return <>
+        <Stack alignItems={'center'} spacing={2} p={2} m={2}>
+            <GlassCard>
+                <Typography variant={'h4'} fontWeight={'bold'} textAlign={'center'}>
+                    Whatever you were looking for, can't be found. 404
+                </Typography>
+                <Typography textAlign={'center'}>
+                    Why don't you check out our actual pages instead?
+                </Typography>
+                <Stack width={'100%'} alignItems={'center'} direction={'row'} spacing={2}>
+                    {/* TODO: I feel I should move these into their own components.. */}
+                    <Button variant='contained'>
+                        HOME
+                    </Button>
+                    <Button variant='contained'>
+                        ABOUT
+                    </Button>
+                </Stack>
+
+            </GlassCard>
+
+        </Stack>
+
+    </>
+}
 
 export default NotFound;
